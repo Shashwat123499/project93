636d3a6a3a51cd9b207485fd93be7451f646d674
Shashwat123499 <84261321+Shashwat123499@users.noreply.github.com>
2023-10-23 19:47:11 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBw2GaiD77UXU-PuNktiSg9p7zZEFQ9Kx8",
+    authDomain: "project-93-c1d9c.firebaseapp.com",
+    databaseURL: "https://project-93-c1d9c-default-rtdb.firebaseio.com",
+    projectId: "project-93-c1d9c",
+    storageBucket: "project-93-c1d9c.appspot.com",
+    messagingSenderId: "565957592329",
+    appId: "1:565957592329:web:ccfcfc065e8017d0173fd8"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
