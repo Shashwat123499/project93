5a5ddd0e42314bd2a2fe93825dcd8b0ced2f33a1
Shashwat123499 <84261321+Shashwat123499@users.noreply.github.com>
2023-10-23 19:45:57 +0530

    Update kwitter.js

--- kwitter.js
+++ kwitter.js
@@ -2,5 +2,5 @@ function addUser() {
     user_name = document.getElementById("user_name");
     localStorage.setItem("user_name", user_name);
 
-    window.location = "kwitter_room.js";
+    window.location = "kwitter_room.html";
 }
